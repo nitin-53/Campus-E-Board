559dc0e85857bf1b199793566f32c7bc6199a20f
nvjot <navjot627@gmail.com>
2016-05-08 13:05:37 +0530

    d n

---
+++ app/src/main/java/xyz/leapmind/ceb/campus_e_board/AppControl/AppConfig.java
@@ -0,0 +1,12 @@
+package xyz.leapmind.ceb.campus_e_board.AppControl;
+
+/**
+ * Created by nitin on 25/4/16.
+ */
+public class AppConfig {
+    // Server user login url
+    public static String URL_LOGIN = "http://leapmind.xyz/Login.php";
+
+    // Server user register url
+    public static String URL_REGISTER = "http://leapmind.xyz/Register.php";
+}
